4e1f5ee38125cf6621be6da4c8a77279b08e300e
Nikolay Zhelyazkov <nikizhelqzkov@gmail.com>
2020-04-01 16:44:53 +0300

    commit

--- Employee.h
+++ Employee.h
@@ -1,6 +1,23 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 
+class Employee
+{
+private:
+    /* data */
+public:
+    Employee(/* args */);
+    ~Employee();
+};
+
+Employee::Employee(/* args */)
+{
+}
+
+Employee::~Employee()
+{
+}
+
 
 
 #endif
