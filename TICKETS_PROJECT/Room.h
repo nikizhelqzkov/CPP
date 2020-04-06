9d2fc59a2c10957b13317743436dba9cb058c449
Nikolay Zhelyazkov <nikizhelqzkov@gmail.com>
2020-04-06 03:04:54 +0300

    ideas

--- TICKETS_PROJECT/Room.h
+++ TICKETS_PROJECT/Room.h
@@ -5,16 +5,38 @@ class Room
 {
 private:
     int id;
+//mestata shte sa ot obekti    
     int **places;
     int rows;
     int placeOnRow;
 
 public:
     Room();
-    Room(int,  int **,int, int);
+    Room(int, int **, int, int);
     Room(const Room &);
     ~Room();
-    void print()const;
+    void print() const;
+    //getur
+    //seturi
+    //i->ti element
+    //operaciq ravno
+    //==
+    //!=
 };
 
+class RoomArr
+{
+private:
+    Room *arr;
+    size_t size;
+
+public:
+    RoomArr();
+    RoomArr(Room *, size_t);
+    RoomArr(const RoomArr&);
+    ~RoomArr();
+    void printArr()const;
+    //i-ti element
+    //add event = operator + na room i proverka dali tozi nomer na staqta ne e zaet
+};
 #endif
